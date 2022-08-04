28a8d9d46bb833ca51011f05e685e25ddb7adad8
Arvid Bushati <arvid.b901@gmail.com>
2022-08-03 20:36:35 -0400

    Update DCP---product-of-all-the-numbers-in-the-original-array.py

--- DCP---product-of-all-the-numbers-in-the-original-array.py
+++ DCP---product-of-all-the-numbers-in-the-original-array.py
@@ -16,3 +16,30 @@ test_arrays = [[3, 2, 1], [1, 2, 3, 4, 5]]
 for arr in test_arrays:
     new_arr = get_new_array(arr)
     print(f'Input arr {arr}, Output arr {new_arr}')
+    
+    
+    
+    
+    
+ class Solution:
+    def productExceptSelf(self, n: List[int]) -> List[int]:
+
+        
+        length = len(n)
+        left = [1] * length
+        for i in range(1,length):
+            left[i] = left[i-1] * n[i-1]
+        # print(left)
+        
+        right = [1] * length
+        for i in range(length-1,0,-1):
+            right[i-1] = right[i] * n[i]
+
+        
+        output = []
+        for i in range(length):
+            output.append(left[i]*right[i])
+        
+        return output
+        
+        
